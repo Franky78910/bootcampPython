d290931e3c0e02aaa8316aca5eba9edeaa1fd2fd
Franklin <frankyr@misena.edu.co>
2024-08-21 19:30:55 -0500

    aboradmos input

---
+++ Inputs/input_numeros.py
@@ -0,0 +1,7 @@
+#pedirle un numero al usuario
+numero = input("dame un numero cam´pista: ")
+#multiplicaion por 2
+#convirtiendo el numero entero a float
+resultado = float(numero) *3
+#imprimiendo 
+print (resultado)
