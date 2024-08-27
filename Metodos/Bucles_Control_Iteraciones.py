c574f3b4868084b40a7889058ec153fac120ef27
Franky78910 <frankyr@misena.edu.co>
2024-08-26 19:07:27 -0500

    Add files via upload

---
+++ Metodos/Bucles_Control_Iteraciones.py
@@ -0,0 +1,22 @@
+numbers =[1,2,3,4,5,6]
+for i in numbers:
+    print("aqui i es igual a: ",i+1)
+for i in range (3, 10):
+    print(i)    
+fruits = ["Manzana","Pera","Fresa","Naranja","Mandarina","Melon"]
+for fruta in fruits:
+    print(fruta)
+    if fruta == "Fresa":
+        print("Fresa encontrada")
+        
+x= 0
+while x<5:
+    if x ==3:
+        break
+    print(x)
+    x+=1
+numbers =[1,2,3,4,5,6]
+for i in numbers:
+    if i == 3:
+      break
+    print("aqui i es igual a: ",i+1)
