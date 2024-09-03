0cc5ce008b39cadc12e320047a918caf49bbd9a9
Franklin <frankyr@misena.edu.co>
2024-09-02 20:04:13 -0500

    uso de funciones

---
+++ Funciones/uso-de-funciones.py
@@ -0,0 +1,8 @@
+def greet(name, last_name):
+    print("hola", name, last_name="No tiene apellido")
+greet("Edward", "Ramirez")
+greet("Franklin") 
+greet(last_name="Ramirez", name="Franklin")
+
+
+   
